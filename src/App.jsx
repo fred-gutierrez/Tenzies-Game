91c60647e6b0b30c5ac1570777a35fe35fb6d892
Fred <fredsg22@hotmail.com>
2023-01-22 00:17:40 -0600

    number generating array added functions and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-import "./styles/main.css";
+import { useState } from "react";
+import Die from "./components/Die";
 
 export default function App() {
-  return <main></main>;
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  return (
+    <main>
+      <section className="die-container">
+        {dice.map((die) => (
+          <Die value={die} />
+        ))}
+      </section>
+    </main>
+  );
 }
