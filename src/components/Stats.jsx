7ebd34f21e2765061f95a5e9643599ba4b3d516c
Fred <fredsg22@hotmail.com>
2023-02-07 17:57:02 -0600

    updated styles for best and current sections

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,12 +3,23 @@ import React from "react";
 export default function Stats(props) {
   return (
     <div className="stats-container">
-      <section>
-        <p>Rolls: {props.rollClicks}</p>
-        <p>Current time: {props.formattedTime}</p>
+      <section className="current-section">
+        <p>
+          Rolls: <span className="current-rolls">{props.rollClicks}</span>
+        </p>
+        <p>
+          Current time:{" "}
+          <span className="current-time">{props.formattedTime}</span>
+        </p>
       </section>
-      <section>
-        {props.bestRoll ? <p>Best rolls: {props.bestRoll}</p> : ""}
+      <section className="best-section">
+        {props.bestRoll ? (
+          <p>
+            Best rolls: <span className="best-rolls">{props.bestRoll}</span>
+          </p>
+        ) : (
+          ""
+        )}
         {/* {props.bestTime ? <p>Best time: {props.bestTime}</p> : ""} */}
       </section>
     </div>
